0d2c70388b451b5b240c5359927ea98e548d1d03
metsson <merkur@live.se>
2015-01-26 12:41:04 +0100

    Delete faulty live as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   # The (5) most used search terms
   def search_trends
-    Search.where('count > ? AND updated_at > ?', 10, Time.now - 10.days)
+    Search.where('count > ? AND updated_at > ?', 10, Time.now - 10.days).take(5)
     #Search.all.order('count DESC').take(5)
   end
 end
